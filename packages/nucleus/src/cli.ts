7eecb0b9886ea7e6b65270d9e1c9825decde24a5
Chiel Kunkels <chiel@kunkels.me>
2022-06-22 00:52:53 +0200

    fix(nucleus): add handler for uncaught exceptions

--- packages/nucleus/src/cli.ts
+++ packages/nucleus/src/cli.ts
@@ -1,6 +1,13 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+import { logger } from './utils';
+
+process.on('uncaughtException', function(err) {
+	logger.error(err.message);
+	process.exit(1);
+});
+
 yargs(hideBin(process.argv))
 	.usage('$0 <command> [options]')
 	.demandCommand()
