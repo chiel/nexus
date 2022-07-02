2562b28a906513b09c454d5de6d49d5ed7d85b3e
Chiel Kunkels <chiel@kunkels.me>
2022-07-02 16:32:15 +0200

    fix(nucleus): add basic html template

--- packages/nucleus/src/webpack/plugins/__tests__/getHtmlWebpackPlugin.spec.ts
+++ packages/nucleus/src/webpack/plugins/__tests__/getHtmlWebpackPlugin.spec.ts
@@ -1,12 +1,16 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-import getHtmlWebpackPlugin from '../getHtmlWebpackPlugin';
+import getHtmlWebpackPlugin, { DEFAULT_TEMPLATE } from '../getHtmlWebpackPlugin';
 
 jest.mock('html-webpack-plugin');
 
 describe('getHtmlWebpackPlugin', () => {
 	it('should return a HtmlWebpackPlugin', () => {
 		const result = getHtmlWebpackPlugin();
+		expect(HtmlWebpackPlugin).toHaveBeenCalledWith({
+			publicPath: '/',
+			templateContent: DEFAULT_TEMPLATE,
+		});
 		expect(result).toBeInstanceOf(HtmlWebpackPlugin);
 	});
 });
--- packages/nucleus/src/webpack/plugins/getHtmlWebpackPlugin.ts
+++ packages/nucleus/src/webpack/plugins/getHtmlWebpackPlugin.ts
@@ -1,5 +1,20 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+export const DEFAULT_TEMPLATE = `<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Page Title</title>
+	</head>
+	<body>
+		<div id="app-root"></div>
+	</body>
+</html>`;
+
 export default function getHtmlWebpackPlugin() {
-	return new HtmlWebpackPlugin({ publicPath: '/' });
+	return new HtmlWebpackPlugin({
+		publicPath: '/',
+		templateContent: DEFAULT_TEMPLATE,
+	});
 }
