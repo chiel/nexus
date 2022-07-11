de9dbfe9ecbf1098337e959f6f18fa128ce38751
Chiel Kunkels <chiel@kunkels.me>
2022-07-12 01:12:58 +0200

    fix(nucleus): allow templates without files

--- packages/nucleus/src/utils/__tests__/applyTemplate.spec.ts
+++ packages/nucleus/src/utils/__tests__/applyTemplate.spec.ts
@@ -1,9 +1,11 @@
 import chalk from 'chalk';
+import { access } from 'fs/promises';
 import path from 'path';
 
 import applyTemplate from '../applyTemplate';
 import copyDirectory from '../copyDirectory';
 
+jest.mock('fs/promises');
 jest.mock('../copyDirectory');
 
 describe('applyTemplate', () => {
@@ -11,21 +13,28 @@ describe('applyTemplate', () => {
 
 	beforeEach(() => {
 		jest.spyOn(console, 'info').mockImplementation();
+		(access as jest.Mock).mockRejectedValue(new Error('Boop'));
 		(copyDirectory as jest.Mock).mockImplementation();
 	});
 
-	it('should call copy directory for the template files directory', async () => {
+	it('should log a message for the template being applied', async () => {
 		await applyTemplate('base', '/dest');
 
-		expect(console.info).toHaveBeenCalledWith('Apply template', chalk.green('base'));
+		expect(console.info).toHaveBeenCalledWith('Applying template', chalk.green('base'));
+	});
+
+	it('should copy the template files directory if it exists', async () => {
+		(access as jest.Mock).mockResolvedValue(undefined);
+		await applyTemplate('base', '/dest');
+
 		expect(copyDirectory).toHaveBeenCalledWith(`${src}/files`, '/dest', expect.any(Function));
 	});
 
 	it('should log a message for each file being processed and return the contents unaltered', async () => {
+		(access as jest.Mock).mockResolvedValue(undefined);
 		await applyTemplate('base', '/dest');
 
 		const [[,, fp]] = (copyDirectory as jest.Mock).mock.calls;
-
 		fp(`${src}/files/.npmrc`, 'beepboop');
 		expect(console.info).toHaveBeenCalledWith('    ...copying', chalk.green('.npmrc'));
 	});
--- packages/nucleus/src/utils/applyTemplate.ts
+++ packages/nucleus/src/utils/applyTemplate.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import { access } from 'fs/promises';
 import path from 'path';
 
 import copyDirectory from './copyDirectory';
@@ -6,11 +7,21 @@ import copyDirectory from './copyDirectory';
 export default async function applyTemplate(name: string, dest: string): Promise<void> {
 	const src = path.join(__dirname, 'templates', name);
 
-	console.info('Apply template', chalk.green(name));
+	console.info('Applying template', chalk.green(name));
 
+	let hasFiles = false;
 	const files = path.join(src, 'files');
-	await copyDirectory(files, dest, (file, contents) => {
-		console.info('    ...copying', chalk.green(file.replace(`${files}/`, '')));
-		return contents;
-	});
+	try {
+		await access(files);
+		hasFiles = true;
+	} catch (_err: unknown) {
+		// fall-through
+	}
+
+	if (hasFiles) {
+		await copyDirectory(files, dest, (file, contents) => {
+			console.info('    ...copying', chalk.green(file.replace(`${files}/`, '')));
+			return contents;
+		});
+	}
 }
